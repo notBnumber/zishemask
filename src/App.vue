9004f5a2653b23ef73e4f7991c521ae8dcafab7a
Dortmund_com <15625738634@163.com>
2019-01-30 14:16:05 +0800

    2019年1月30日14:16:01
    分享

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,17 @@ export default {
   onLaunch() {
     console.log("小程序初始化执行");
     let vm = this;
-    wx.getSetting({      
+    wx.getSetting({
       success(res) {
-        if(!res.authSetting["scope.userInfo"]) {
+        if (!res.authSetting["scope.userInfo"]) {
           // 未授权, 则跳转至授权页
-          vm.replaceTo('/pages/login/wxLogin');
-        }else {
+          vm.replaceTo("/pages/login/wxLogin");
+        } else {
           wx.login({
             success(res) {
-              console.log(res,'授权');
-              
-              wx.setStorageSync('code', res.code)
+              console.log(res, "授权");
+
+              wx.setStorageSync("code", res.code);
               // vm.$API.wxLogin({
               //   code: res.code
               // }).then(response => {
@@ -21,14 +21,31 @@ export default {
               //   wx.switchTab({url: '/pages/purchase/purchase'})
               // })
             }
-          })
+          });
         }
       }
-    })
-  }
-}
+    });
+  },
+  // onShareAppMessage: function(ops) {
+  //   if (ops.from === "button") {
+  //     // 来自页面内转发按钮
+  //     console.log(ops.target);
+  //   }
+  //   return {
+  //     title: "紫色魁影",
+  //     path: "pages/home/index",
+  //     success: function(res) {
+  //       // 转发成功
+  //       console.log("转发成功:" + JSON.stringify(res));
+  //     },
+  //     fail: function(res) {
+  //       // 转发失败
+  //       console.log("转发失败:" + JSON.stringify(res));
+  //     }
+  //   };
+  // }
+};
 </script>
 
 <style>
-
 </style>
