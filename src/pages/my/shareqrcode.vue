4992c45e7fe2d89d3e6b4f4d44bc16a73cf3a014
Dortmund_com <15625738634@163.com>
2019-01-30 15:38:13 +0800

    2019年1月30日15:38:04
    触底

--- src/pages/my/shareqrcode.vue
+++ src/pages/my/shareqrcode.vue
@@ -9,55 +9,66 @@
 </template>
 
 <script>
-    let qrcodeplugin=requirePlugin("poster");
+let qrcodeplugin = requirePlugin("poster");
 export default {
   data() {
     return {
       isDownRefresh: false,
-        savebtnText:'点击保存',
-        drawinginfo:[]
+      savebtnText: "点击保存",
+      drawinginfo: []
     };
   },
-    methods:{
-        setActive(index) {
-            this.tabActive = index;
-        },
-    },
-    onLoad() {
-        this.drawinginfo=[{
-                type: 'image',
-                url: 'https://jin.itxiaolong.cn/icon/index.jpg',
-                left: 0,
-                top: 0,
-                width: 650,
-                height: 950
-            },
-            {
-                type: 'text',
-                textType: 'CN',
-                content: '我是文字',
-                fontSize: 26,
-                color: 'white',
-                textAlign: 'left',
-                left: 170,
-                top: 50,
-                width: 650,
-            }];
+  methods: {
+    setActive(index) {
+      this.tabActive = index;
     }
+  },
+  onShow() {
+    console.log(111);
 
+    this.drawinginfo = [
+      {
+        type: "image",
+        url: "https://jin.itxiaolong.cn/icon/index.jpg",
+        left: 0,
+        top: 0,
+        width: 650,
+        height: 950
+      },
+      {
+        type: "text",
+        textType: "CN",
+        content: "我是文字",
+        fontSize: 26,
+        color: "white",
+        textAlign: "center",
+        left: 170,
+        top: 50,
+        width: 650
+      }
+    ];
+  }
 };
 </script>
 
 <style scoped lang='scss'>
-  .bodys{
-    width: 100%;
-  }
-  /* 画布样式 */
-  .canvas-style{
-    width: 100% !important;
-    height: 700px  !important;
-    margin: 0 auto;
-    border: 1px solid orangered;
-    margin-top: 3px;
-  }
+.bodys {
+  width: 100%;
+}
+/* 画布样式 */
+.canvas-style {
+  // width: 100% !important;
+  // height: 700px  !important;
+  // margin: 0 auto;
+  // border: 1px solid orangered;
+  // margin-top: 3px;
+  width: 650rpx !important;
+  height: 950rpx !important;
+  margin: 0 auto;
+  border: 1px solid orangered;
+  margin-top: 10rpx;
+}
+.palette {
+  margin: auto;
+}
 </style>
