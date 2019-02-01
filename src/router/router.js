ecc0fa55410736295ade06748e62d1a84799d8c0
itxiaolong3 <1710600973@qq.com>
2019-02-01 20:33:46 +0800

    Revert "引入海报插件"
    This reverts commit 598da0d8e4108af7fc9d860de030542999c5841a.

--- src/router/router.js
+++ src/router/router.js
@@ -421,6 +421,60 @@ module.exports = [
             }
         }
     },
+    {
+        path: "pages/my/shareqrcode",
+        config: {
+            "navigationBarTitleText": "我的专属码",
+            "navigationBarTextStyle": "white",
+            "navigationBarBackgroundColor": "#E9323C",
+            "usingComponents": {
+                "i-modal": "/iView/modal/index",
+                "i-swipeout": "/iView/swipeout/index",
+                "i-toast": "/iView/toast/index",
+                "i-spin": "/iView/spin/index",
+                "poster": "plugin://poster/poster"
+            }
+        }
+    },
+    {
+        path: "pages/my/teaminfo",
+        config: {
+            "navigationBarTitleText": "队员信息",
+            "navigationBarTextStyle": "white",
+            "navigationBarBackgroundColor": "#E9323C",
+            "usingComponents": {
+                "i-modal": "/iView/modal/index",
+                "i-swipeout": "/iView/swipeout/index",
+                "i-toast": "/iView/toast/index",
+            }
+        }
+    },
+    {
+        path: "pages/my/earnings",
+        config: {
+            "navigationBarTitleText": "累积收益",
+            "navigationBarTextStyle": "white",
+            "navigationBarBackgroundColor": "#E9323C",
+            "usingComponents": {
+                "i-modal": "/iView/modal/index",
+                "i-swipeout": "/iView/swipeout/index",
+                "i-toast": "/iView/toast/index",
+            }
+        }
+    },
+    {
+        path: "pages/my/earningsdetail",
+        config: {
+            "navigationBarTitleText": "收益详细",
+            "navigationBarTextStyle": "white",
+            "navigationBarBackgroundColor": "#E9323C",
+            "usingComponents": {
+                "i-modal": "/iView/modal/index",
+                "i-swipeout": "/iView/swipeout/index",
+                "i-toast": "/iView/toast/index",
+            }
+        }
+    },
   {
     path: "pages/my/power",
     config: {
