f76378dc40fa5d25add71a83e457a43bc58bc92f
Dortmund_com <15625738634@163.com>
2019-02-13 11:46:52 +0800

    sj2019年2月13日11:46:43
    单选

--- src/pages/my/userinfo.vue
+++ src/pages/my/userinfo.vue
@@ -8,12 +8,12 @@
                     <span>姓名：</span>
                     <input title="姓名" placeholder="请输入名字" v-model="info.user_name" />
                 </div>
-                <!-- <RadioGroup v-model="info.sex" @change="changeModel" class="sexradio">
+                <RadioGroup @change="changeModel" class="sexradio">
                     <span>性别：</span>
-                    <Radio :label="0" :value="0" class="myradio">男</Radio>
-                    <Radio :label="1" :value="1" class="myradio">女</Radio>
-                </RadioGroup> -->
-                <div class="inp">
+                    <Radio :label="0" :value="0" class="myradio"  v-model="fuckDefault">男</Radio>
+                    <Radio :label="1" :value="1" class="myradio"  v-model="fuckDefault">女</Radio>
+                </RadioGroup>
+                <!-- <div class="inp">
                     <span>
                         性别:
                     </span>
@@ -21,7 +21,8 @@
                         <input type="radio" :value="index" v-model="fuckDefault">
                         <span>{{items.title}}</span>
                     </div>
-                </div>
+
+                </div> -->
                 <div class="inp">
                     <span>
                         QQ：
