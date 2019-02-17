9e51de917aebda76d1594b899b0c0bbe45d9f1c9
Dortmund_com <15625738634@163.com>
2019-02-18 01:53:11 +0800

    2019年2月18日01:53:06
    更新

--- src/pages/my/index.vue
+++ src/pages/my/index.vue
@@ -57,8 +57,8 @@
           <span>待收货</span>
         </span>
         <!--<span class="body-item" @click="pageTo('/pages/my/orderCenter', {tabIndex: 4})">-->
-          <!--<i class="evaluate"></i>-->
-          <!--<span>待评价</span>-->
+        <!--<i class="evaluate"></i>-->
+        <!--<span>待评价</span>-->
         <!--</span>-->
         <span class="body-item" @click="pageTo('/pages/my/orderAfterSale')">
           <i class="refund"></i>
@@ -69,11 +69,11 @@
 
     <div class="box">
       <ul>
-        <!--<li @click="pageTo('/pages/my/myEvaluate')">-->
-          <!--<span>-->
-            <!--<i class="evaluate"></i>我的足迹</span>-->
-          <!--<i class="arrow"></i>-->
-        <!--</li>-->
+        <li @click="pageTo('/pages/my/dayday')">
+          <span>
+            <i class="evaluate"></i>签到</span>
+          <i class="arrow"></i>
+        </li>
         <li @click="pageTo('/pages/my/myCollection')">
           <span>
             <i class="Collection"></i>收藏商品</span>
@@ -94,25 +94,26 @@
             <i class="setting"></i>编辑资料</span>
           <i class="arrow"></i>
         </li>
-        <li @click="pageTo('/pages/my/power')">
-          <span>
-            <i class="myRight"></i>我的特权</span>
-          <i class="arrow"></i>
-        </li>
         <li @click="pageTo('/pages/my/pyramid')">
           <span>
             <i class="distribution"></i>分销中心</span>
           <i class="arrow"></i>
         </li>
+        <li @click="pageTo('/pages/my/power')">
+          <span>
+            <i class="myRight"></i>我的特权</span>
+          <i class="arrow"></i>
+        </li>
+
         <li @click="docall">
           <span>
             <i class="complaints"></i>投诉:{{toustel}}</span>
           <i class="arrow"></i>
         </li>
         <!--<li @click="xieyi">-->
-          <!--<span>-->
-            <!--<i class="complaints"></i>我是协议</span>-->
-          <!--<i class="arrow"></i>-->
+        <!--<span>-->
+        <!--<i class="complaints"></i>我是协议</span>-->
+        <!--<i class="arrow"></i>-->
         <!--</li>-->
       </ul>
       <div class="outLogin" @click="outLogin">
@@ -129,14 +130,14 @@ export default {
   mixins: [modal],
   data() {
     return {
-        ID:'',
-        level:'',
+      ID: "",
+      level: "",
       avatarUrl: "",
       nickName: "",
       modal1: false,
       timer: null,
       isLogin: false,
-        toustel:''
+      toustel: ""
     };
   },
   methods: {
@@ -144,94 +145,94 @@ export default {
       this.replaceTo("/pages/login/wxLogin");
       //  wx.setStorageSync('sessionId', response.data.sessionId)
 
-      wx.clearStorage()
+      wx.clearStorage();
     },
     getUserInfo() {
       let vm = this;
-        this.$API
-            .tousucall({
-                i: 2,
-                c: "entry",
-                a: "wxapp",
-                m: "mask",
-                do: "Qrcodeinfo",
-                uid: wx.getStorageSync("sessionId")
-            })
-            .then(res => {
-                console.log(res, "头像昵称");
-                if (res.code == 1) {
-                    vm.avatarUrl = res.data.headerimg;
-                    vm.nickName = res.data.nickName;
-                    vm.ID = res.data.id;
-                    vm.level = res.data.level;
-                }
-            });
+      this.$API
+        .tousucall({
+          i: 2,
+          c: "entry",
+          a: "wxapp",
+          m: "mask",
+          do: "Qrcodeinfo",
+          uid: wx.getStorageSync("sessionId")
+        })
+        .then(res => {
+          console.log(res, "头像昵称");
+          if (res.code == 1) {
+            vm.avatarUrl = res.data.headerimg;
+            vm.nickName = res.data.nickName;
+            vm.ID = res.data.id;
+            vm.level = res.data.level;
+          }
+        });
       //以下方法微信即将废弃
-//      wx.getUserInfo({
-//        success(res) {
-//          vm.avatarUrl = res.userInfo.avatarUrl;
-//          vm.nickName = res.userInfo.nickName;
-//        },
-//        fail(err) {
-//          wx.showToast({
-//            title: "获取用户信息失败",
-//            icon: "none"
-//          });
-//          vm.timer = setTimeout(() => {
-//            vm.modalOpen("modal1");
-//          }, 1500);
-//        }
-//      });
+      //      wx.getUserInfo({
+      //        success(res) {
+      //          vm.avatarUrl = res.userInfo.avatarUrl;
+      //          vm.nickName = res.userInfo.nickName;
+      //        },
+      //        fail(err) {
+      //          wx.showToast({
+      //            title: "获取用户信息失败",
+      //            icon: "none"
+      //          });
+      //          vm.timer = setTimeout(() => {
+      //            vm.modalOpen("modal1");
+      //          }, 1500);
+      //        }
+      //      });
     },
-      docall(){
-        let that=this;
-          wx.makePhoneCall({
-              phoneNumber: that.toustel //仅为示例，并非真实的电话号码
-          })
-      },
-      getcall(){
-          this.$API
-              .tousucall({
-                  i: 2,
-                  c: "entry",
-                  a: "wxapp",
-                  m: "mask",
-                  do: "Tousu",
-                  uid: wx.getStorageSync("sessionId")
-              })
-              .then(res => {
-                  console.log(res, "投诉电话");
-                  if (res.code == 1) {
-                      this.toustel = res.data.tel;
-                  }
-              });
-      },
-      xieyi(){
-//          wx.showActionSheet({
-//              itemList: ['类型1', '类型二', '类型三'],
-//              success(res) {
-//                  console.log(res.tapIndex)
-//                  wx.showToast({
-//                          title: '选择了'+res.tapIndex,
-//                          duration: 1000
-//                      })
-//              },
-//              fail(res) {
-//                  console.log(res.errMsg)
-//              }
-//          })
-      },
-//    getSetting() {
-//      let vm = this;
-//      wx.getSetting({
-//        success(res) {
-//          console.log(res, "是否授权");
-//          if (!res.authSetting["scope.userInfo"]) {
-//            vm.getUserInfo();
-//          }
-//        }
-//      });
-//    },
+    docall() {
+      let that = this;
+      wx.makePhoneCall({
+        phoneNumber: that.toustel //仅为示例，并非真实的电话号码
+      });
+    },
+    getcall() {
+      this.$API
+        .tousucall({
+          i: 2,
+          c: "entry",
+          a: "wxapp",
+          m: "mask",
+          do: "Tousu",
+          uid: wx.getStorageSync("sessionId")
+        })
+        .then(res => {
+          console.log(res, "投诉电话");
+          if (res.code == 1) {
+            this.toustel = res.data.tel;
+          }
+        });
+    },
+    xieyi() {
+      //          wx.showActionSheet({
+      //              itemList: ['类型1', '类型二', '类型三'],
+      //              success(res) {
+      //                  console.log(res.tapIndex)
+      //                  wx.showToast({
+      //                          title: '选择了'+res.tapIndex,
+      //                          duration: 1000
+      //                      })
+      //              },
+      //              fail(res) {
+      //                  console.log(res.errMsg)
+      //              }
+      //          })
+    },
+    //    getSetting() {
+    //      let vm = this;
+    //      wx.getSetting({
+    //        success(res) {
+    //          console.log(res, "是否授权");
+    //          if (!res.authSetting["scope.userInfo"]) {
+    //            vm.getUserInfo();
+    //          }
+    //        }
+    //      });
+    //    },
     goSetting(name) {
       let vm = this;
       wx.openSetting({
@@ -247,15 +248,15 @@ export default {
     }
   },
   onShow() {
-      console.log('onShow')
-      this.getcall();
+    console.log("onShow");
+    this.getcall();
     if (wx.getStorageSync("sessionId")) {
       this.isLogin = true;
     }
     this.getUserInfo();
   },
   onUnload() {
-      console.log('onUnload')
+    console.log("onUnload");
     clearTimeout(this.timer);
   }
 };
@@ -274,7 +275,7 @@ export default {
   }
   .head {
     height: 80px;
-    background-color: #ED555F;
+    background-color: #ed555f;
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -307,7 +308,7 @@ export default {
         .shenfen {
           display: flex;
           justify-content: flex-start;
-          span{
+          span {
             font-size: 16px;
             padding: 5px 1px;
           }
@@ -408,7 +409,6 @@ export default {
           &.refund {
             background-image: url("~@/assets/img/user_icon_5@2x.png");
           }
-
         }
         span {
           font-size: 12px;
@@ -490,7 +490,6 @@ export default {
       border-radius: 8px;
       color: #fff;
     }
-
   }
 }
 </style>
