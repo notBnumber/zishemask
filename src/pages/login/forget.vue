e92f4c17fd810a6c887ce7b6c461fb6c733d6e0e
itxiaolong3 <1710600973@qq.com>
2019-03-21 09:28:01 +0800

    忘记密码取消手机号格式验证

--- src/pages/login/forget.vue
+++ src/pages/login/forget.vue
@@ -50,7 +50,8 @@ export default {
       password: "",
       repeatpass: "",
       isfocu: null,
-      phoneRegexp: /^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199|(147))\d{8}$/,
+      //phoneRegexp: /^((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199|(147))\d{8}$/,
+      phoneRegexp: /^[1][0-9][0-9]{9}$/,
         message: "获取验证码",
         timeState: false,
         getphone:''
